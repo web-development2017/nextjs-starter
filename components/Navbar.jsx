42434cd3276bc59d7403681d96bcf470f7c61441
Rich <web_dev2017@outlook.com>
2022-07-13 19:28:53 +0100

    forgot to add environment variables to vercel

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,46 +3,51 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Navbar(){
     const { data: session, status } = useSession();
-    
-    if(status === "loading"){
-        return(
-            <div className="p-10 mx-auto max-w-4xl justify-center">
-                <nav>
-                    <h1>Example</h1>
-                    <ul>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                        
-                    </ul>
-                </nav>
-            </div>
-        )    
-    }else if (status === "unauthenticated") { 
-        return (
-            <div className="p-10 mx-auto max-w-4xl justify-center">
-                <nav>
-                    <h1>Example</h1>
-                    <ul>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                        <li onClick={ signIn }><a href="">Login</a></li>                    
-                    </ul>
-                </nav>
-            </div>
-        )      
-        
-    } else if(status === "authenticated"){
-        return (
-            <div className="p-10 mx-auto max-w-4xl justify-center">
-                <nav>
-                    <h1>Example</h1>
-                    <ul>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                        <li><Link href="/restricted"><a>Account</a></Link></li>
-                        <li onClick={() => signOut({ callbackUrl: 'http://localhost:3000' }) }><Link href="/"><a>Logout</a></Link></li>                    
-                    </ul>
-                </nav>
-            </div>
-        )
+    if(session){
+        return <p>got session</p>
     }else{
-        return <p></p>
+        return <p>haven't got session</p>
     }
+    
+    // if(status === "loading"){
+    //     return(
+    //         <div className="p-10 mx-auto max-w-4xl justify-center">
+    //             <nav>
+    //                 <h1>Example</h1>
+    //                 <ul>
+    //                     <li><Link href="/"><a>Home</a></Link></li>
+                        
+    //                 </ul>
+    //             </nav>
+    //         </div>
+    //     )    
+    // }else if (status === "unauthenticated") { 
+    //     return (
+    //         <div className="p-10 mx-auto max-w-4xl justify-center">
+    //             <nav>
+    //                 <h1>Example</h1>
+    //                 <ul>
+    //                     <li><Link href="/"><a>Home</a></Link></li>
+    //                     <li onClick={ signIn }><a href="">Login</a></li>                    
+    //                 </ul>
+    //             </nav>
+    //         </div>
+    //     )      
+        
+    // } else if(status === "authenticated"){
+    //     return (
+    //         <div className="p-10 mx-auto max-w-4xl justify-center">
+    //             <nav>
+    //                 <h1>Example</h1>
+    //                 <ul>
+    //                     <li><Link href="/"><a>Home</a></Link></li>
+    //                     <li><Link href="/restricted"><a>Account</a></Link></li>
+    //                     <li onClick={() => signOut({ callbackUrl: 'http://localhost:3000' }) }><Link href="/"><a>Logout</a></Link></li>                    
+    //                 </ul>
+    //             </nav>
+    //         </div>
+    //     )
+    // }else{
+    //     return <p></p>
+    // }
 }
